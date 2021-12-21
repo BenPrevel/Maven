2eac866574251f2681ceb7926ee2019b834c70e6
BenPrevel <403rutabaga@gmail.com>
2021-12-21 20:15:32 +0100

    Maven quest done

---
+++ src/test/java/com/wcs/app/AppTest.java
@@ -0,0 +1,20 @@
+package com.wcs.app;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
